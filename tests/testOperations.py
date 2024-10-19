bea8818cd89c356259219902173331dfde0e9e65
Priyank N <44031169+PriyanK7n@users.noreply.github.com>
2024-10-19 10:04:21 -0400

    Updated Files with Errors Fixed

--- tests/testOperations.py
+++ tests/testOperations.py
@@ -1,10 +1,13 @@
 from src.mathOperations import add, sub, multiply, divide
+import pytest
+
 
 def test_add():
     assert add(2, 3) == 5
-    assert add (5, 9) == 14
-    assert add (-11, 11) == 0
-    assert add (0, 10000) == 10000'
+    assert add(5, 9) == 14
+    assert add(-11, 11) == 0
+    assert add(0, 10000) == 10000
+
 
 def test_sub():
     assert sub(5, 2) == 3
@@ -12,11 +15,13 @@ def test_sub():
     assert sub(-11, 11) == -22
     assert sub(0, 5) == -5
 
+
 def test_multiply():
     assert multiply(4, 6) == 24
     assert multiply(0, 6) == 0
     assert multiply(0, -11) == 0
     assert multiply(-11, -11) == 121
+
 
 def test_divide():
     assert divide(10, 2) == 5
